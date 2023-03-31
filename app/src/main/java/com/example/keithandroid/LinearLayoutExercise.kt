56115571a752e0fee7caeb9422f85bfdef2c771c
Brokeice123 <keithmarkomen@gmail.com>
2023-03-31 13:21:14 +0300

    Linear layout and Relative Layout

---
+++ app/src/main/java/com/example/keithandroid/LinearLayoutExercise.kt
@@ -0,0 +1,11 @@
+package com.example.keithandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearLayoutExercise : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout_exercise)
+    }
+}
