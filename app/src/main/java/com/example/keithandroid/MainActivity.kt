a28d588afecc10b7b3eadde47f7d71a8b497ee47
Brokeice123 <keithmarkomen@gmail.com>
2023-04-03 13:35:46 +0300

    Added toast message to the login button

--- app/src/main/java/com/example/keithandroid/MainActivity.kt
+++ app/src/main/java/com/example/keithandroid/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +22,8 @@ class MainActivity : AppCompatActivity() {
         loginBtn = findViewById(R.id.login_loginbtn)
         regBtn = findViewById(R.id.login_registerbtn)
 
-
+        loginBtn.setOnClickListener {
+            Toast.makeText(this, "I just got Clicked!!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
