dd21824be471052e51c7634193119f7b93e7fbef
Brokeice123 <keithmarkomen@gmail.com>
2023-04-03 13:10:33 +0300

    Added id's to elements inlogin activity and register activity.

--- app/src/main/java/com/example/keithandroid/MainActivity.kt
+++ app/src/main/java/com/example/keithandroid/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.keithandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var email:EditText
+    private lateinit var password:EditText
+    private lateinit var loginBtn:Button
+    private lateinit var regBtn:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        email = findViewById(R.id.login_email)
+        password = findViewById(R.id.login_password)
+        loginBtn = findViewById(R.id.login_loginbtn)
+        regBtn = findViewById(R.id.login_registerbtn)
+
+
     }
 }
